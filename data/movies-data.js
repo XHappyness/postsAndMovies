4054bd017355310c8938fafc677ea3a65e9502e6
xuehaishuang <xuehaishuang@gridsum.com>
2018-03-23 18:08:12 +0800

    准备电影详情页面的数据

--- data/movies-data.js
+++ data/movies-data.js
@@ -6,21 +6,99 @@ var inTheaters = {
       "title": "古墓丽影：源起之战",
       "stars": "35",
       "average": 6.4,
-      "image": "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2512717509.jpg"
+      "image": "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2512717509.jpg",
+
+      "country": '美国',
+      "originalTitle": "Pacific Rim: Uprising",
+      "wishCount": 15970,
+      "commentCount": 17692,
+      "year": '2018',
+      "generes": '动作、冒险',
+      "director": "罗阿尔·乌索格",
+      "casts": '艾丽西亚·维坎德/多米尼克·威斯特/沃尔顿·戈金斯/吴彦祖',
+      "castsInfo": [{
+        "name": "艾丽西亚·维坎德",
+        "image": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1427204716.36.jpg"
+        },
+        {
+          "name": "多米尼克·威斯特",
+          "image": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p4228.jpg"
+        },
+        {
+          "name": "沃尔顿·戈金斯",
+          "image": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p50351.jpg"
+        },
+        {
+          "name": "吴彦祖",
+          "image": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p23544.jpg"
+        }],
+      "summary": "劳拉·克劳馥年少时，她的冒险家父亲突然神秘消失。她拒绝接受父亲已经去世的事实，并暗下决心要解开父亲神秘死亡的谜团。\n放下眼前的一切，劳拉前往父亲最后出现的地方探寻——一座传说中的古墓，位于日本附近某个神秘岛上。要踏上这场未知的旅程，她必须以敏锐的头脑、坚定的信仰和顽强的精神武装自己，并迫使自己超越极限。如果她能在这场危机四伏的冒险中存活下来，她才可能成长为名副其实的——“古墓丽影”。"
     },
     {
       "movieId": "26752852",
       "title": "水形物语",
       "stars": "40",
       "average": 7.3,
-      "image": "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2515650989.jpg"
+      "image": "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2515650989.jpg",
+      
+      "country": '美国',
+      "originalTitle": "Pacific Rim: Uprising",
+      "wishCount": 15970,
+      "commentCount": 17692,
+      "year": '2018',
+      "generes": '动作、冒险',
+      "director": "罗阿尔·乌索格",
+      "casts": '艾丽西亚·维坎德/多米尼克·威斯特/沃尔顿·戈金斯/吴彦祖',
+      "castsInfo": [{
+        "name": "艾丽西亚·维坎德",
+        "image": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1427204716.36.jpg"
+      },
+      {
+        "name": "多米尼克·威斯特",
+        "image": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p4228.jpg"
+      },
+      {
+        "name": "沃尔顿·戈金斯",
+        "image": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p50351.jpg"
+      },
+      {
+        "name": "吴彦祖",
+        "image": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p23544.jpg"
+      }],
+      "summary": "劳拉·克劳馥年少时，她的冒险家父亲突然神秘消失。她拒绝接受父亲已经去世的事实，并暗下决心要解开父亲神秘死亡的谜团。\n放下眼前的一切，劳拉前往父亲最后出现的地方探寻——一座传说中的古墓，位于日本附近某个神秘岛上。要踏上这场未知的旅程，她必须以敏锐的头脑、坚定的信仰和顽强的精神武装自己，并迫使自己超越极限。如果她能在这场危机四伏的冒险中存活下来，她才可能成长为名副其实的——“古墓丽影”。"
     },
     {
       "movieId": "6390825",
       "title": "黑豹",
       "stars": "35",
       "average": 6.7,
-      "image": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2512123434.jpg"
+      "image": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2512123434.jpg",
+      
+      "country": '美国',
+      "originalTitle": "Pacific Rim: Uprising",
+      "wishCount": 15970,
+      "commentCount": 17692,
+      "year": '2018',
+      "generes": '动作、冒险',
+      "director": "罗阿尔·乌索格",
+      "casts": '艾丽西亚·维坎德/多米尼克·威斯特/沃尔顿·戈金斯/吴彦祖',
+      "castsInfo": [{
+        "name": "艾丽西亚·维坎德",
+        "image": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1427204716.36.jpg"
+      },
+      {
+        "name": "多米尼克·威斯特",
+        "image": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p4228.jpg"
+      },
+      {
+        "name": "沃尔顿·戈金斯",
+        "image": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p50351.jpg"
+      },
+      {
+        "name": "吴彦祖",
+        "image": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p23544.jpg"
+      }],
+      "summary": "劳拉·克劳馥年少时，她的冒险家父亲突然神秘消失。她拒绝接受父亲已经去世的事实，并暗下决心要解开父亲神秘死亡的谜团。\n放下眼前的一切，劳拉前往父亲最后出现的地方探寻——一座传说中的古墓，位于日本附近某个神秘岛上。要踏上这场未知的旅程，她必须以敏锐的头脑、坚定的信仰和顽强的精神武装自己，并迫使自己超越极限。如果她能在这场危机四伏的冒险中存活下来，她才可能成长为名副其实的——“古墓丽影”。"
     }
   ]
 }
@@ -36,21 +114,99 @@ var comingSoons = {
       "title": "环太平洋：雷霆再起",
       "stars": "00",
       "average": 0,
-      "image": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2512983475.jpg"
+      "image": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2512983475.jpg",
+      
+      "country": '美国',
+      "originalTitle": "Pacific Rim: Uprising",
+      "wishCount": 15970,
+      "commentCount": 17692,
+      "year": '2018',
+      "generes": '动作、冒险',
+      "director": "罗阿尔·乌索格",
+      "casts": '艾丽西亚·维坎德/多米尼克·威斯特/沃尔顿·戈金斯/吴彦祖',
+      "castsInfo": [{
+        "name": "艾丽西亚·维坎德",
+        "image": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1427204716.36.jpg"
+      },
+      {
+        "name": "多米尼克·威斯特",
+        "image": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p4228.jpg"
+      },
+      {
+        "name": "沃尔顿·戈金斯",
+        "image": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p50351.jpg"
+      },
+      {
+        "name": "吴彦祖",
+        "image": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p23544.jpg"
+      }],
+      "summary": "劳拉·克劳馥年少时，她的冒险家父亲突然神秘消失。她拒绝接受父亲已经去世的事实，并暗下决心要解开父亲神秘死亡的谜团。\n放下眼前的一切，劳拉前往父亲最后出现的地方探寻——一座传说中的古墓，位于日本附近某个神秘岛上。要踏上这场未知的旅程，她必须以敏锐的头脑、坚定的信仰和顽强的精神武装自己，并迫使自己超越极限。如果她能在这场危机四伏的冒险中存活下来，她才可能成长为名副其实的——“古墓丽影”。"
     },
     {
       "movieId": "26661194",
       "title": "脱皮爸爸",
       "stars": "30",
       "average": 6.1,
-      "image": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2516154772.jpg"
+      "image": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2516154772.jpg",
+
+      "country": '美国',
+      "originalTitle": "Pacific Rim: Uprising",
+      "wishCount": 15970,
+      "commentCount": 17692,
+      "year": '2018',
+      "generes": '动作、冒险',
+      "director": "罗阿尔·乌索格",
+      "casts": '艾丽西亚·维坎德/多米尼克·威斯特/沃尔顿·戈金斯/吴彦祖',
+      "castsInfo": [{
+        "name": "艾丽西亚·维坎德",
+        "image": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1427204716.36.jpg"
+      },
+      {
+        "name": "多米尼克·威斯特",
+        "image": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p4228.jpg"
+      },
+      {
+        "name": "沃尔顿·戈金斯",
+        "image": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p50351.jpg"
+      },
+      {
+        "name": "吴彦祖",
+        "image": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p23544.jpg"
+      }],
+      "summary": "劳拉·克劳馥年少时，她的冒险家父亲突然神秘消失。她拒绝接受父亲已经去世的事实，并暗下决心要解开父亲神秘死亡的谜团。\n放下眼前的一切，劳拉前往父亲最后出现的地方探寻——一座传说中的古墓，位于日本附近某个神秘岛上。要踏上这场未知的旅程，她必须以敏锐的头脑、坚定的信仰和顽强的精神武装自己，并迫使自己超越极限。如果她能在这场危机四伏的冒险中存活下来，她才可能成长为名副其实的——“古墓丽影”。"
     },
     {
       "movieId": "26828415",
       "title": "青春24秒",
       "stars": "00",
       "average": 0,
-      "image": "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2512337269.jpg"
+      "image": "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2512337269.jpg",
+
+      "country": '美国',
+      "originalTitle": "Pacific Rim: Uprising",
+      "wishCount": 15970,
+      "commentCount": 17692,
+      "year": '2018',
+      "generes": '动作、冒险',
+      "director": "罗阿尔·乌索格",
+      "casts": '艾丽西亚·维坎德/多米尼克·威斯特/沃尔顿·戈金斯/吴彦祖',
+      "castsInfo": [{
+        "name": "艾丽西亚·维坎德",
+        "image": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1427204716.36.jpg"
+      },
+      {
+        "name": "多米尼克·威斯特",
+        "image": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p4228.jpg"
+      },
+      {
+        "name": "沃尔顿·戈金斯",
+        "image": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p50351.jpg"
+      },
+      {
+        "name": "吴彦祖",
+        "image": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p23544.jpg"
+      }],
+      "summary": "劳拉·克劳馥年少时，她的冒险家父亲突然神秘消失。她拒绝接受父亲已经去世的事实，并暗下决心要解开父亲神秘死亡的谜团。\n放下眼前的一切，劳拉前往父亲最后出现的地方探寻——一座传说中的古墓，位于日本附近某个神秘岛上。要踏上这场未知的旅程，她必须以敏锐的头脑、坚定的信仰和顽强的精神武装自己，并迫使自己超越极限。如果她能在这场危机四伏的冒险中存活下来，她才可能成长为名副其实的——“古墓丽影”。"
     }
   ]
 };
@@ -63,21 +219,99 @@ var top250 = {
       "title": "肖申克的救赎",
       "stars": "50",
       "average": 9.6,
-      "image": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p480747492.jpg"
+      "image": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p480747492.jpg",
+
+      "country": '美国',
+      "originalTitle": "Pacific Rim: Uprising",
+      "wishCount": 15970,
+      "commentCount": 17692,
+      "year": '2018',
+      "generes": '动作、冒险',
+      "director": "罗阿尔·乌索格",
+      "casts": '艾丽西亚·维坎德/多米尼克·威斯特/沃尔顿·戈金斯/吴彦祖',
+      "castsInfo": [{
+        "name": "艾丽西亚·维坎德",
+        "image": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1427204716.36.jpg"
+      },
+      {
+        "name": "多米尼克·威斯特",
+        "image": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p4228.jpg"
+      },
+      {
+        "name": "沃尔顿·戈金斯",
+        "image": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p50351.jpg"
+      },
+      {
+        "name": "吴彦祖",
+        "image": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p23544.jpg"
+      }],
+      "summary": "劳拉·克劳馥年少时，她的冒险家父亲突然神秘消失。她拒绝接受父亲已经去世的事实，并暗下决心要解开父亲神秘死亡的谜团。\n放下眼前的一切，劳拉前往父亲最后出现的地方探寻——一座传说中的古墓，位于日本附近某个神秘岛上。要踏上这场未知的旅程，她必须以敏锐的头脑、坚定的信仰和顽强的精神武装自己，并迫使自己超越极限。如果她能在这场危机四伏的冒险中存活下来，她才可能成长为名副其实的——“古墓丽影”。"
     },
     {
       "movieId": "1291546",
       "title": "霸王别姬",
       "stars": "50",
       "average": 9.5,
-      "image": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p1910813120.jpg"
+      "image": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p1910813120.jpg",
+
+      "country": '美国',
+      "originalTitle": "Pacific Rim: Uprising",
+      "wishCount": 15970,
+      "commentCount": 17692,
+      "year": '2018',
+      "generes": '动作、冒险',
+      "director": "罗阿尔·乌索格",
+      "casts": '艾丽西亚·维坎德/多米尼克·威斯特/沃尔顿·戈金斯/吴彦祖',
+      "castsInfo": [{
+        "name": "艾丽西亚·维坎德",
+        "image": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1427204716.36.jpg"
+      },
+      {
+        "name": "多米尼克·威斯特",
+        "image": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p4228.jpg"
+      },
+      {
+        "name": "沃尔顿·戈金斯",
+        "image": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p50351.jpg"
+      },
+      {
+        "name": "吴彦祖",
+        "image": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p23544.jpg"
+      }],
+      "summary": "劳拉·克劳馥年少时，她的冒险家父亲突然神秘消失。她拒绝接受父亲已经去世的事实，并暗下决心要解开父亲神秘死亡的谜团。\n放下眼前的一切，劳拉前往父亲最后出现的地方探寻——一座传说中的古墓，位于日本附近某个神秘岛上。要踏上这场未知的旅程，她必须以敏锐的头脑、坚定的信仰和顽强的精神武装自己，并迫使自己超越极限。如果她能在这场危机四伏的冒险中存活下来，她才可能成长为名副其实的——“古墓丽影”。"
     },
     {
       "movieId": "1295644",
       "title": "这个杀手不太冷",
       "stars": "50",
       "average": 9.4,
-      "image": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p511118051.jpg"
+      "image": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p511118051.jpg",
+
+      "country": '美国',
+      "originalTitle": "Pacific Rim: Uprising",
+      "wishCount": 15970,
+      "commentCount": 17692,
+      "year": '2018',
+      "generes": '动作、冒险',
+      "director": "罗阿尔·乌索格",
+      "casts": '艾丽西亚·维坎德/多米尼克·威斯特/沃尔顿·戈金斯/吴彦祖',
+      "castsInfo": [{
+        "name": "艾丽西亚·维坎德",
+        "image": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1427204716.36.jpg"
+      },
+      {
+        "name": "多米尼克·威斯特",
+        "image": "https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p4228.jpg"
+      },
+      {
+        "name": "沃尔顿·戈金斯",
+        "image": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p50351.jpg"
+      },
+      {
+        "name": "吴彦祖",
+        "image": "https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p23544.jpg"
+      }],
+      "summary": "劳拉·克劳馥年少时，她的冒险家父亲突然神秘消失。她拒绝接受父亲已经去世的事实，并暗下决心要解开父亲神秘死亡的谜团。\n放下眼前的一切，劳拉前往父亲最后出现的地方探寻——一座传说中的古墓，位于日本附近某个神秘岛上。要踏上这场未知的旅程，她必须以敏锐的头脑、坚定的信仰和顽强的精神武装自己，并迫使自己超越极限。如果她能在这场危机四伏的冒险中存活下来，她才可能成长为名副其实的——“古墓丽影”。"
     }
   ]
 };
--- pages/movies/movie-detail/movie-detail.js
+++ pages/movies/movie-detail/movie-detail.js
@@ -6,7 +6,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    currntMovie: {}
   },
 
   /**
@@ -22,5 +22,8 @@ Page({
         break;
       }
     }
+    this.setData({
+      currntMovie: currntMovie
+    });
   }
 })
